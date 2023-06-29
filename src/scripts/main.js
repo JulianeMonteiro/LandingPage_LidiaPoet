81e4da40e88fd8783ef57d5e84917cb9174a118e
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-06-29 15:33:26 -0300

    Secoes header, quote e footer criadas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+
+  const heroSection = document.querySelector(".hero");
+  const alturaHero = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+      ocultaElementosDoHeader();
+    } else {
+      exibeElementosDoHeader();
+    }
+  });
+});
+
+function ocultaElementosDoHeader() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function exibeElementosDoHeader() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
+}
